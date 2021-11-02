6d098b9a2273ebaf3e71eb1b3c3c5455128743e1
Jobot <jojo64364@gmail.com>
2021-11-03 00:22:37 +0100

    début du programme

---
+++ etml_people_counter.ino
@@ -0,0 +1,35 @@
+//Auteur : José Carlos Gasser
+//Date : 02.11.2021
+//Description : tout ce qu'il faut se trouve là dedans
+
+
+// ---------------------------------------------------------------- //
+// Arduino Ultrasoninc Sensor HC-SR04
+// Re-writed by José Carlos Gasser
+// Using Arduino IDE 1.8.16
+// Using HC-SR04 Module
+// Tested on 2 Novembre 2021
+// ---------------------------------------------------------------- //
+
+
+//le pin 10 s'attache au pin ECHO du sensor
+int echoPin 10;
+
+//la pin 8 s'attache au pin Trig du sensor
+int trigPin 8;
+
+//variable de la mesure du temps de voyage des ondes
+long duration;
+
+//variable de la distance mesurée
+int distance;
+
+void setup()
+{
+  
+}
+
+void loop()
+{
+  
+}
