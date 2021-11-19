296a3c4ca329705b7629d761ec4a94b07d330905
J0bot <jojo64364@gmail.com>
2021-11-19 11:47:13 +0100

    on a essayé de faire l'algo, mais on est nuls

--- main_stuff/main_stuff.ino
+++ main_stuff/main_stuff.ino
@@ -36,6 +36,13 @@ MySQL_Connection conn((Client *)&client);
 //redPin c'est la 1
 //yellow c'est la A1
 
+//define de states of the lasers
+#define R 1
+#define L 2
+#define LR 3
+static int state_index = 0;
+int states[4];
+
 
 //La fonction setup va se lancer une seule fois
 void setup() {
@@ -44,6 +51,8 @@ void setup() {
 
   pinMode(1, OUTPUT);
   pinMode(0, OUTPUT);
+
+  memset(states, 0, 4);
 
 
   // Ici on va essayer de se connecter au wifi
@@ -108,8 +117,8 @@ void loop() {
   while (true)
   {
     //Ici on va check si les gens rentrent ou sortent
-    Serial.print("Pin A5 : ");
-    Serial.println(analogRead(A5));
+    //Serial.print("Pin A5 : ");
+    //Serial.println(analogRead(A5));
     if(analogRead(A5)<70)
     {
       digitalWrite(0, HIGH);  
@@ -118,8 +127,8 @@ void loop() {
     {
       digitalWrite(0, LOW);
     }
-    Serial.print("Pin A6 : ");
-    Serial.println(analogRead(A6));
+    //Serial.print("Pin A6 : ");
+    //Serial.println(analogRead(A6));
     if(analogRead(A6)<70)
     {
       digitalWrite(1, HIGH);  
@@ -129,15 +138,24 @@ void loop() {
       digitalWrite(1, LOW);
     }
 
-    //Si laser : 31, si pas laser : 1023
-    
-    char* recType = "exit"; //c'est le type d'info qu'on envoie, entry ou exit
+   int result = checklasers();
+   if(result == L)
+   {
+        char* recType = "entry"; //c'est le type d'info qu'on envoie, entry ou exit
+        Serial.println("entry");
+        insert_record(ardMacAddress, recType);
 
-      delay(1000); //delai de 10 secondes entre chaque entry pour le debug
-    if (recType == "exit" || recType == "entry")
-    {
-      insert_record(ardMacAddress, recType);
-    }
+   }
+
+   if(result == R)
+   {
+        char* recType = "exit"; //c'est le type d'info qu'on envoie, entry ou exit
+        Serial.println("exit");
+        insert_record(ardMacAddress, recType);
+   }
+    
+    //treshhold de 70
+    
   }
   conn.close(); //Si on a tout finit, on ferme la connection
   Serial.println("finished my job");
@@ -213,4 +231,96 @@ void insert_record(char* ardMacAddress, char* recType)
 
   executeQuery(request); //On execute la fonction executeQuery
   free(request);
+}
+
+int checklasers()
+{
+  int state;
+  if(analogRead(A5)<70 && analogRead(A6)<70 )
+    state = LR;
+
+  if(analogRead(A5)<70 && analogRead(A6)>70 )
+    state = L;
+
+  if(analogRead(A5)>70 && analogRead(A6)<70 )
+    state = R;
+
+  if(analogRead(A5)>70 && analogRead(A6)>70 )
+    state = 0;
+
+
+
+  if(state_index == 0) {
+    states[state_index] = state;
+    state_index++;
+
+    if(state_index==4)
+    {
+      state_index = 0;  
+    }
+  }
+  /*
+  if(states[state_index-1] == state)
+  {
+    return 0;
+  }
+
+  
+  if(state_index == 4)
+  { 
+    state_index = 0; 
+    
+    states[state_index] = state;
+    state_index++;
+
+  }
+  else
+  {
+    states[state_index] = state;
+    state_index++;
+
+
+  }
+    Serial.print("State_index : ");
+    Serial.println(state_index-1);
+
+    Serial.print("State : ");
+    Serial.println(state);
+
+    Serial.print("states : ");
+    Serial.print(states[0]);
+    Serial.print(", ");
+    Serial.print(states[1]);
+    Serial.print(", ");
+    Serial.print(states[2]);
+    Serial.print(", ");
+    Serial.println(states[3]);
+  
+    if (state == LR) {
+    int sum_states = 0;
+    for(int i = 0 ; i < state_index ; i++)
+    {
+      sum_states+=states[i];
+    }
+
+    memset(states, 0, 4);
+
+    Serial.println(sum_states);
+    if(sum_states==6)
+    {
+
+      Serial.println("Detected entry of exit");
+        if(states[0] == R || states[3] == R)
+        {
+          return R;
+        }
+        else if(states[0] == L || states[3] == L)
+        {
+          return L;
+        }
+    }
+    }
+    
+  return 0;
+  */
 }
