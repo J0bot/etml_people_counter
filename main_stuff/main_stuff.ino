7adb441624cf3a4506fe4028e2498a9c9e20438f
J0bot <jojo64364@gmail.com>
2021-11-20 18:09:00 +0100

    Changé les include

--- main_stuff/main_stuff.ino
+++ main_stuff/main_stuff.ino
@@ -3,9 +3,7 @@
  * Date : 12.12.2021
  * Description : programme qui va être flash sur l'arduino et qui commande tout
  */
-#include <iostream>
-#include <string>
-#include <algorithm>
+
 #include <SPI.h>
 #include <WiFiNINA.h>
 #include <MySQL_Connection.h>
@@ -32,10 +30,6 @@ WiFiClient client;
 //L'objet conn de la classe mysql_connection fait référence à l'objet WifiClient
 MySQL_Connection conn((Client *)&client);
 
-//pins 
-//redPin c'est la 1
-//yellow c'est la A1
-
 //define de states of the lasers
 #define R 1
 #define L 2
@@ -44,102 +38,7 @@ static int state_index = 0;
 int states[4];
 
 
-//La fonction setup va se lancer une seule fois
-void setup() {
-  //Initialisation du bail pour avoir le port console série
-  Serial.begin(9600);
-
-  pinMode(1, OUTPUT);
-  pinMode(0, OUTPUT);
-
-  memset(states, 0, 4);
-
-
-  // Ici on va essayer de se connecter au wifi
-  while ( status != WL_CONNECTED) {
-    status = WiFi.begin(ssid, pass);
-
-    // wait 5 seconds for connection to the wifi
-    delay(5000);
-  }
-
-  //Connection to mysql database
-  Serial.println("Connecting...");
-  if (conn.connect(server_addr, 3306, user, password)) {
-
-    //Petit delay de 1 secondes pour laisser le temps de se connecter
-    delay(1000);
-    Serial.println("Connection successful !");
-
-    //Rajouter son addresse mac si elle n'existe pas dans la table t_arduino
-    
-    //On va attraper l'addresse mac de l'arduino
-    ardMacAddress = getMacAddress();
-    
-
-    //On va afficher l'addresse mac pour du debug
-    Serial.println(ardMacAddress);
-
-    // Formater toute la query
-    char* mysql_query = "INSERT IGNORE INTO db_pretpi.t_arduino SET ardMacAddress=\'"; //On va ecrire la query dans les values
-    int string_length = sizeof(char)*1024; //on set la longeur du string à 1024 bytes
-    char* request = (char*)malloc(string_length);  //On va allouer le string_length à notre requete
-    int index = 0;
-    while(mysql_query[index]!='\0') {
-      request[index++] = mysql_query[index];
-    }
-    int index2 = 0;
-    while(ardMacAddress[index2] != '\0') {
-      request[index++] = ardMacAddress[index2++];
-    }
-    request[index++] = '\'';
-    request[index] = '\0';
-
-    //on va print la requete mysql sur la console série pour le debug
-    Serial.println(request);
-
-    //On execute la query
-    executeQuery(request);
-    free(request);
-  }
-  else
-  {
-    // Si la connection a vraiment fail on va afficher qu'elle a fail
-    Serial.println("Connection failed.");
-  }
-}
-
-// Cette fonction va tourner tout le temps
-void loop() {
-  
-  while (true)
-  {
-    blink_leds();
-    int result = checklasers();
-    if(result == L)
-    {
-      char* recType = "entry"; //c'est le type d'info qu'on envoie, entry ou exit
-      Serial.println("entry");
-      insert_record(ardMacAddress, recType);
-    }
-
-    if(result == R)
-    {
-      char* recType = "exit"; //c'est le type d'info qu'on envoie, entry ou exit
-      Serial.println("exit");
-      insert_record(ardMacAddress, recType);
-    }    
-  }
-  conn.close(); //Si on a tout finit, on ferme la connection
-  Serial.println("finished my job");
-}
-
-
-
-//Cette fonction va retourner la mac address :)
-//c'est la seule fonction qui a été prise de l'ancien projet x) 
-//Bon j'ai refait la fonction avec std::string, j'ai fait un peu au hasard mais ça compile du coup jsp
-//Au final on est allé sur du char avec emixam
+//Cette fonction va retourner la mac address
 char* getMacAddress() {
   byte mac[6]; //c'est un tableau de byte de taille 6 de la librairie arduino
   WiFi.macAddress(mac); //Cette fonction assigne la mac address à mon tableau de byte
@@ -161,7 +60,6 @@ char* getMacAddress() {
 }
 
 
-
 //C'est la fonction qui nous permet d'executer une query
 void executeQuery(const char mysql_query[])
 {
@@ -173,7 +71,6 @@ void executeQuery(const char mysql_query[])
   // Deleting the cursor also frees up memory used
   delete cur_mem;
 }
-
 
 
 //cette fonction permet d'inserer une donnée dans la table t_record
@@ -211,7 +108,6 @@ void insert_record(char* ardMacAddress, char* recType)
   executeQuery(request); //On execute la fonction executeQuery
   free(request);
 }
-
 
 
 // Cette fonction va checker si des gens entrent ou sortent
@@ -289,7 +185,6 @@ int checklasers()
       Serial.println("Detected entry of exit");
       if(states[previous_index] == R)
       {
-        
         return R;
       }
       else if(states[previous_index] == L)
@@ -314,4 +209,98 @@ void blink_leds()
     digitalWrite(1, HIGH);  
   else
     digitalWrite(1, LOW);
+}
+
+
+//La fonction setup va se lancer une seule fois
+void setup() {
+  //Initialisation du bail pour avoir le port console série
+  Serial.begin(9600);
+
+  Serial.println("Hello this is taxi");
+  
+  pinMode(1, OUTPUT);
+  pinMode(0, OUTPUT);
+
+  memset(states, 0, 4);
+
+
+  // Ici on va essayer de se connecter au wifi
+  while ( status != WL_CONNECTED) {
+    status = WiFi.begin(ssid, pass);
+
+    // wait 5 seconds for connection to the wifi
+    delay(5000);
+  }
+
+  //Connection to mysql database
+  Serial.println("Connecting...");
+  if (conn.connect(server_addr, 3306, user, password)) {
+
+    //Petit delay de 1 secondes pour laisser le temps de se connecter
+    delay(1000);
+    Serial.println("Connection successful !");
+    
+    
+    ///Rajouter son addresse mac si elle n'existe pas dans la table t_arduino
+    
+    //On va attraper l'addresse mac de l'arduino
+    ardMacAddress = getMacAddress();
+    
+
+    //On va afficher l'addresse mac pour du debug
+    Serial.println(ardMacAddress);
+
+    // Formater toute la query
+    char* mysql_query = "INSERT IGNORE INTO db_pretpi.t_arduino SET ardMacAddress=\'"; //On va ecrire la query dans les values
+    int string_length = sizeof(char)*1024; //on set la longeur du string à 1024 bytes
+    char* request = (char*)malloc(string_length);  //On va allouer le string_length à notre requete
+    int index = 0;
+    while(mysql_query[index]!='\0') {
+      request[index++] = mysql_query[index];
+    }
+    int index2 = 0;
+    while(ardMacAddress[index2] != '\0') {
+      request[index++] = ardMacAddress[index2++];
+    }
+    request[index++] = '\'';
+    request[index] = '\0';
+
+    //on va print la requete mysql sur la console série pour le debug
+    Serial.println(request);
+
+    //On execute la query
+    executeQuery(request);
+    free(request);
+  }
+  else
+  {
+    // Si la connection a vraiment fail on va afficher qu'elle a fail
+    Serial.println("Connection failed.");
+  }
+}
+
+// Cette fonction va tourner tout le temps
+void loop() {
+  
+  while (true)
+  {
+    blink_leds();
+    int result = checklasers();
+    if(result == L)
+    {
+      char* recType = "entry"; //c'est le type d'info qu'on envoie, entry ou exit
+      Serial.println("entry");
+      insert_record(ardMacAddress, recType);
+    }
+
+    if(result == R)
+    {
+      char* recType = "exit"; //c'est le type d'info qu'on envoie, entry ou exit
+      Serial.println("exit");
+      insert_record(ardMacAddress, recType);
+    }    
+  }
+  conn.close(); //Si on a tout finit, on ferme la connection
+  Serial.println("finished my job");
 }
