3a0adbfeb0af7f31ea8ff6d10e6fe72490c3ea71
pietro <78962489+kuoch00@users.noreply.github.com>
2021-11-24 15:49:37 +0100

    ajout bouton (clic non fonctionnel)
    pour vider les entrées

--- www/ProjetPreTPI/src/php/index.php
+++ www/ProjetPreTPI/src/php/index.php
@@ -48,6 +48,10 @@
         <div class="row ml-1 mt-3">
             <div><input id="toggleCharts" type="checkbox" checked data-toggle="toggle" data-on="Temps réel" data-off="Graphiques" data-onstyle="info" data-offstyle="primary" onclick="toggleCharts()"></div>
             <div class="col"></div>
+            <!--implementation bouton vider entrées-->
+            <div>
+                <input type="button" value="Vider toutes les entrées." class="btn btn-danger"> 
+            </div>
         </div>
         <div class="row row-cols-xl-3 row-cols-lg-2 row-cols-md-2 row-cols-sm-1 row-cols-1">
             <?php
